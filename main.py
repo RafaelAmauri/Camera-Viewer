a60dd85451f9e3a8854e05412a3dce264f34c955
Rafael <49131143+RafaelAmauri@users.noreply.github.com>
2024-10-25 16:03:32 -0300

    Update main.py

--- main.py
+++ main.py
@@ -49,11 +49,25 @@ def updateImage(window, cam, model, userChoices):
     ]
 
 
-    while True:
-        picture = openCamera(cam)
-        if picture is not None:
-            results = model.predict(picture, classes=[0], verbose=False)
-            for result in results:
+    picture = openCamera(cam)
+    picture = cv2.resize(picture, (540, 400), interpolation=cv2.INTER_LANCZOS4)
+    if picture is not None:
+        results = model.predict(picture, classes=[0], verbose=False)
+
+
+        # Edge Detect
+        if userChoices['EdgeDetectOn']:
+            grad_x = cv2.Sobel(picture, cv2.CV_16S, 1, 0, ksize=3, scale=1, delta=0, borderType=cv2.BORDER_DEFAULT)
+            grad_y = cv2.Sobel(picture, cv2.CV_16S, 0, 1, ksize=3, scale=1, delta=0, borderType=cv2.BORDER_DEFAULT)
+                
+            abs_grad_x = cv2.convertScaleAbs(grad_x)
+            abs_grad_y = cv2.convertScaleAbs(grad_y)
+            picture = cv2.addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0)
+
+
+        for result in results:
+            # Show KeyPoints
+            if userChoices['KeyPointsOn']:
                 for kp in result.keypoints:
                     for detection in kp.xy:
                         previousPoint = -1
@@ -63,18 +77,16 @@ def updateImage(window, cam, model, userChoices):
                                 if previousPoint != -1:
                                     cv2.line(picture, (int(x), int(y)), previousPoint, color=kpColors[idx], thickness=2)
                                 previousPoint = (int(x), int(y))
+            # Show Bounding Box
+            if userChoices['BoundingBoxOn']:
+                for obj in result.boxes:
+                    for x0, y0, x1, y1 in obj.xyxy:
+                        cv2.rectangle(picture, (int(x0), int(y0)), (int(x1), int(y1)), color=(255, 255, 0), thickness=2)
 
-                if userChoices['BoundingBoxOn']:
-                    for obj in result.boxes:
-                        for x0, y0, x1, y1 in obj.xyxy:
-                            cv2.rectangle(picture, (int(x0), int(y0)), (int(x1), int(y1)), color=(255, 255, 0), thickness=2)
 
-            picture_data = numpyToImg(picture)
-            window['-IMAGE-'].update(data=picture_data)
-
-            window.refresh()
-        time.sleep(0.1)  # Add a small delay to reduce CPU usage
-
+        picture_data = numpyToImg(picture)
+        window['-IMAGE-'].update(data=picture_data)
+        window.refresh()
 
 
 def mainWindow():
@@ -87,7 +99,8 @@ def mainWindow():
 
     userChoices = {
                     'BoundingBoxOn': False,
-                    #'KeyPointsOn': False
+                    'KeyPointsOn': False,
+                    'EdgeDetectOn': False
                 }
     
     if not cam.isOpened():
@@ -98,35 +111,49 @@ def mainWindow():
     placeholder = np.random.randint(0, 255, (640, 480, 3), dtype=np.uint8)
     picture_data = numpyToImg(placeholder)
 
+    layoutLeft  = [[sg.Image(data=picture_data, key='-IMAGE-')]]
+    layoutMiddle = [
+                    [sg.Button('Ligar caixas de detecção', key='-ENABLE-BOUNDING-BOX-'), sg.Button('Desligar caixas de detecção', key='-DISABLE-BOUNDING-BOX-')],
+                    [sg.Button('Ligar marcadores de corpo', key='-ENABLE-KEYPOINTS-'), sg.Button('Desligar marcadores de corpo', key='-DISABLE-KEYPOINTS-')]
+                ]
+    
+    layoutRight = [
+                    [sg.Button('Ligar Detector de Bordas', key='-ENABLE-EDGE-DETECT-'), sg.Button('Desligar Detector de Bordas', key='-DISABLE-EDGE-DETECT-')]
+    ]
+
+
     layout = [
-        [
-            sg.Column([
-                [sg.Text('Image Viewer 1.0')],
-                [sg.Image(data=picture_data, key='-IMAGE-')]
-            ])
-        ],
-        [
-            sg.Column([
-                [sg.Button('Ligar caixa de detecção', key='-ENABLE-BOUNDING-BOX-')],
-                [sg.Button('Desligar caixa de detecção', key='-DISABLE-BOUNDING-BOX-')]
-            ])
-        ]
+                [
+                    sg.vtop(sg.Column(layoutLeft)),   sg.VSeparator(),
+                    sg.vtop(sg.Column(layoutMiddle)), sg.VSeparator(),
+                    sg.vtop(sg.Column(layoutRight))
+                ]
     ]
 
     window = sg.Window('Window Title', layout, resizable=False, finalize=True)
-    threading.Thread(target=updateImage, args=(window, cam, model, userChoices), daemon=True).start()
 
     # Event Loop
     while True:
-        event, values = window.read()
+        event, values = window.read(timeout=7)
         if event == sg.WIN_CLOSED or event == 'Cancel':
             break
 
-        elif event == '-DISABLE-BOUNDING-BOX-':
-            userChoices['BoundingBoxOn'] = False
         elif event == '-ENABLE-BOUNDING-BOX-':
             userChoices['BoundingBoxOn'] = True
+        elif event == '-DISABLE-BOUNDING-BOX-':
+            userChoices['BoundingBoxOn'] = False
 
+        elif event == '-ENABLE-KEYPOINTS-':
+            userChoices['KeyPointsOn'] = True
+        elif event == '-DISABLE-KEYPOINTS-':
+            userChoices['KeyPointsOn'] = False
+
+        elif event == '-ENABLE-EDGE-DETECT-':
+            userChoices['EdgeDetectOn'] = True
+        elif event == '-DISABLE-EDGE-DETECT-':
+            userChoices['EdgeDetectOn'] = False
+
+        updateImage(window, cam, model, userChoices)
 
     window.close()
     cam.release()  # Release the camera when done
